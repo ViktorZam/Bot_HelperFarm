dbdc3e3161403e993e7fe5d5b44a90aa5d1723da
Viktor <42400969+ViktorZam@users.noreply.github.com>
2025-06-14 21:10:40 +0330

    --

--- CalcTarget.py
+++ CalcTarget.py
@@ -26,7 +26,7 @@ class TargetManager:
 
     def FindLocLootObject(self):
         for LootPath in self.LootImgNames:
-            LocObject = self.FindLocObject(LootPath, 0.94)
+            LocObject = self.FindLocObject(LootPath, 0.9)
             if not LocObject is None:
                 break
         
